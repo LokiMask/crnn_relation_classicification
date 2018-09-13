03423af991c79a6638c354159c64051e195981d9
LokiMask <cnsr27@gmail.com>
2018-09-13 14:19:05 +0800

    rnn att add

--- src/models/rnn_model.py
+++ src/models/rnn_model.py
@@ -15,24 +15,21 @@ def linear_layer(name, x, in_size, out_size, is_regularize=False):
       loss_l2 += tf.nn.l2_loss(w) + tf.nn.l2_loss(b)
     return o, loss_l2
 
-def rnn_forward(name, sent_pos, num_filters, keep_prob_rnn):
+def rnn_forward(name, sent_pos, lexical, num_filters, layer_size, keep_prob_rnn):
   with tf.variable_scope(name):
     inputs = sent_pos
-    input_dim = inputs.shape.as_list()[2]
+    max_len = FLAGS.max_len
     inputs = tf.unstack(inputs, num = max_len, axis = 1)
-    lstm_fw_cell = tf.nn.rnn_cell.DropoutWrapper(tf.contrib.rnn.LSTMCell(num_units= num_filters), keep_prob_rnn)
-    lstm_bw_cell = tf.nn.rnn_cell.DropoutWrapper(tf.contrib.rnn.LSTMCell(num_units= num_filters), keep_prob_rnn)
+
+    lstm_fw_cell = tf.nn.rnn_cell.DropoutWrapper(tf.contrib.rnn.MultiRNNCell(
+        [tf.contrib.rnn.LSTMCell(num_units= num_filters) for _ in range(layer_size)]), keep_prob_rnn)
+    lstm_bw_cell = tf.nn.rnn_cell.DropoutWrapper(tf.contrib.rnn.MultiRNNCell(
+        [tf.contrib.rnn.LSTMCell(num_units= num_filters) for _ in range(layer_size)]), keep_prob_rnn)
     outputs, _ , _ = tf.contrib.rnn.static_bidirectional_rnn(lstm_fw_cell,lstm_bw_cell, inputs, dtype=tf.float32)
 
-    inputs = tf.concat(outputs, 2)
-    hidden_size = inputs.shape[2].value
-    w_omega = tf.Variable(tf.random_normal([hidden_size, attention_size],stddev=0.1))
-    b_omega = tf.Variable(tf.random_normal([attention_size],stddev=0.1))
-    v = tf.tanh(tf.tensordot(inputs, w_omega, axes=1) + b_omega)
-    u_omega = tf.Variable(tf.random_normal([100],stddev=0.1))
-    vu = tf.tensordot(v,u_omega,axes=1,name='vu')
-    alphas = tf.nn.softmax(vu,name='alphas')
-    feature = tf.reduce_sum(inputs * tf.expand_dims(alphas, -1 ), 1)
+  feature = outputs[-1]
+  if lexical is not None:
+      feature = tf.concat([lexical, feature], axis=1)
   return feature
 
 
@@ -40,8 +37,8 @@ class RNNModel(BaseModel):
 
   def __init__(self, word_embed, data, word_dim,
               pos_num, pos_dim, num_relations,
-              keep_prob, num_filters, keep_prob_rnn,
-              lrn_rate, is_train):
+              keep_prob, num_filters, layer_size,
+              keep_prob_rnn, lrn_rate, is_train):
     # input data
     lexical, rid, sentence, pos1, pos2 = data
 
@@ -63,12 +60,11 @@ class RNNModel(BaseModel):
     sentence = tf.nn.embedding_lookup(word_embed, sentence)   # batch_size, max_len, word_dim
     pos1 = tf.nn.embedding_lookup(pos1_embed, pos1)       # batch_size, max_len, pos_dim
     pos2 = tf.nn.embedding_lookup(pos2_embed, pos2)       # batch_size, max_len, pos_dim
-    sent_pos = sentence
-    #sent_pos = tf.concat([sentence, pos1, pos2], axis=2)
+    sent_pos = tf.concat([sentence, pos1, pos2], axis=2)
     if is_train:
       sent_pos = tf.nn.dropout(sent_pos, keep_prob)
 
-    feature = rnn_forward('rnn', sent_pos, num_filters, keep_prob_rnn)
+    feature = rnn_forward('rnn', sent_pos, lexical, num_filters, layer_size, keep_prob_rnn)
     feature_size = feature.shape.as_list()[1]
     self.feature = feature
 
@@ -90,7 +86,7 @@ class RNNModel(BaseModel):
     self.logits = logits
     self.prediction = prediction
     self.accuracy = accuracy
-    self.loss = loss_ce + 0.01*loss_l2
+    self.loss = loss_ce + 0.01 * loss_l2
 
     if not is_train:
       return
@@ -104,20 +100,17 @@ class RNNModel(BaseModel):
       self.train_op = optimizer.minimize(self.loss, global_step)
     self.global_step = global_step
 
-
-
-
 def build_train_valid_model(word_embed, train_data, test_data):
   with tf.name_scope("Train"):
     with tf.variable_scope('RNNModel', reuse=None):
-      m_train = RNNModel( word_embed, train_data, FLAGS.word_dim,
+      m_train = RNNModel(word_embed, train_data, FLAGS.word_dim,
                     FLAGS.pos_num, FLAGS.pos_dim, FLAGS.num_relations,
-                    FLAGS.keep_prob, FLAGS.num_filters, FLAGS.keep_prob_rnn,
-                    FLAGS.lrn_rate, is_train=True)
+                    FLAGS.keep_prob, FLAGS.num_filters, FLAGS.layer_size,
+                    FLAGS.keep_prob_rnn, FLAGS.lrn_rate, is_train=True)
   with tf.name_scope('Valid'):
     with tf.variable_scope('RNNModel', reuse=True):
-      m_valid = RNNModel( word_embed, test_data, FLAGS.word_dim,
+      m_valid = RNNModel(word_embed, test_data, FLAGS.word_dim,
                     FLAGS.pos_num, FLAGS.pos_dim, FLAGS.num_relations,
-                    1.0, FLAGS.num_filters, 1.0,
-                    FLAGS.lrn_rate, is_train=False)
+                    1.0, FLAGS.num_filters,FLAGS.layer_size,
+                    1.0, FLAGS.lrn_rate, is_train=False)
   return m_train, m_valid
--- src/models/rnn_model_att.py
+++ src/models/rnn_model_att.py
@@ -15,46 +15,43 @@ def linear_layer(name, x, in_size, out_size, is_regularize=False):
       loss_l2 += tf.nn.l2_loss(w) + tf.nn.l2_loss(b)
     return o, loss_l2
 
-def cnn_forward(name, sent_pos, lexical, num_filters):
+def rnn_forward(name, sent_pos, lexical, num_filters, attention_size, layer_size,  keep_prob_rnn):
   with tf.variable_scope(name):
-    input = tf.expand_dims(sent_pos, axis=-1)
-    input_dim = input.shape.as_list()[2]
+    inputs = sent_pos
+    max_len = FLAGS.max_len
+    inputs = tf.unstack(inputs, num = max_len, axis = 1)
 
-    # convolutional layer
-    pool_outputs = []
-    for filter_size in [2,3,4,5]:
-      with tf.variable_scope('conv-%s' % filter_size):
-        conv_weight = tf.get_variable('W1',
-                            [filter_size, input_dim, 1, num_filters],
-                            initializer=tf.truncated_normal_initializer(stddev=0.1))
-        conv_bias = tf.get_variable('b1', [num_filters],
-                              initializer=tf.constant_initializer(0.1))
-        conv = tf.nn.conv2d(input,
-                            conv_weight,
-                            strides=[1, 1, input_dim, 1],
-                            padding='SAME')
-        conv = tf.nn.relu(conv + conv_bias) # batch_size, max_len, 1, num_filters
-        max_len = FLAGS.max_len
-        pool = tf.nn.max_pool(conv,
-                              ksize= [1, max_len, 1, 1],
-                              strides=[1, max_len, 1, 1],
-                              padding='SAME') # batch_size, 1, 1, num_filters
-        pool_outputs.append(pool)
-    pools = tf.reshape(tf.concat(pool_outputs, 3), [-1, 4*num_filters])
+    lstm_fw_cell = tf.nn.rnn_cell.DropoutWrapper(tf.contrib.rnn.MultiRNNCell(
+        [tf.contrib.rnn.LSTMCell(num_units= num_filters) for _ in range(layer_size)]), keep_prob_rnn)
+    lstm_bw_cell = tf.nn.rnn_cell.DropoutWrapper(tf.contrib.rnn.MultiRNNCell(
+        [tf.contrib.rnn.LSTMCell(num_units= num_filters) for _ in range(layer_size)]), keep_prob_rnn)
+    outputs, _ , _ = tf.contrib.rnn.static_bidirectional_rnn(lstm_fw_cell,lstm_bw_cell, inputs, dtype=tf.float32)
 
-    # feature
-    feature = pools
-    if lexical is not None:
-        feature = tf.concat([lexical, feature], axis=1)
-    return feature
+    w_omega = tf.Variable(tf.random_normal([2 * num_filters, attention_size],stddev=0.1))
+    b_omega = tf.Variable(tf.random_normal([attention_size],stddev=0.1))
+    u_list = []
+    for t in range(max_len):
+        u_t = tf.tanh(tf.matmul(outputs[t], w_omega) + b_omega)
+        u_list.append(u_t)
+    attn_z = []
+    u_omega = tf.Variable(tf.random_normal([attention_size, 1],stddev=0.1))
+    for t in range(max_len):
+        z_t = tf.matmul(u_list[t], u_omega)
+        attn_z.append(z_t)
+    attn_zconcat = tf.concat(attn_z, axis=1)
+    alpha = tf.nn.softmax(attn_zconcat)
+    alpha_trans = tf.reshape(tf.transpose(alpha, [1,0]), [max_len, -1, 1])
+    feature = tf.reduce_sum(outputs * alpha_trans, 0)
+    l2_loss = tf.nn.l2_loss(w_omega) + tf.nn.l2_loss(b_omega) + tf.nn.l2_loss(u_omega)
+  return feature, l2_loss
 
 
-class CNNModel(BaseModel):
+class RNNModel(BaseModel):
 
   def __init__(self, word_embed, data, word_dim,
               pos_num, pos_dim, num_relations,
-              keep_prob, num_filters,
-              lrn_rate, is_train):
+              keep_prob, num_filters, attention_size,
+              layer_size, keep_prob_rnn, lrn_rate, is_train):
     # input data
     lexical, rid, sentence, pos1, pos2 = data
 
@@ -76,13 +73,11 @@ class CNNModel(BaseModel):
     sentence = tf.nn.embedding_lookup(word_embed, sentence)   # batch_size, max_len, word_dim
     pos1 = tf.nn.embedding_lookup(pos1_embed, pos1)       # batch_size, max_len, pos_dim
     pos2 = tf.nn.embedding_lookup(pos2_embed, pos2)       # batch_size, max_len, pos_dim
-
-    # cnn model
     sent_pos = tf.concat([sentence, pos1, pos2], axis=2)
     if is_train:
       sent_pos = tf.nn.dropout(sent_pos, keep_prob)
 
-    feature = cnn_forward('cnn', sent_pos, lexical, num_filters)
+    feature , l2_loss_rnn = rnn_forward('rnn', sent_pos, lexical, num_filters, attention_size,  layer_size, keep_prob_rnn)
     feature_size = feature.shape.as_list()[1]
     self.feature = feature
 
@@ -93,7 +88,7 @@ class CNNModel(BaseModel):
     logits, loss_l2 = linear_layer('linear_cnn', feature,
                                   feature_size, num_relations,
                                   is_regularize=True)
-
+    loss_l2 += l2_loss_rnn
     prediction = tf.nn.softmax(logits)
     prediction = tf.argmax(prediction, axis=1)
     accuracy = tf.equal(prediction, tf.argmax(self.labels, axis=1))
@@ -104,7 +99,7 @@ class CNNModel(BaseModel):
     self.logits = logits
     self.prediction = prediction
     self.accuracy = accuracy
-    self.loss = loss_ce + 0.01*loss_l2
+    self.loss = loss_ce + 0.01 * loss_l2
 
     if not is_train:
       return
@@ -115,24 +110,20 @@ class CNNModel(BaseModel):
 
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     with tf.control_dependencies(update_ops):# for batch_norm
-        self.train_op = optimizer.minimize(self.loss, global_step)
+      self.train_op = optimizer.minimize(self.loss, global_step)
     self.global_step = global_step
 
-
-
-
 def build_train_valid_model(word_embed, train_data, test_data):
-  '''Relation Classification via Convolutional Deep Neural Network'''
   with tf.name_scope("Train"):
-    with tf.variable_scope('CNNModel', reuse=None):
-      m_train = CNNModel( word_embed, train_data, FLAGS.word_dim,
+    with tf.variable_scope('RNNModel', reuse=None):
+      m_train = RNNModel( word_embed, train_data, FLAGS.word_dim,
                     FLAGS.pos_num, FLAGS.pos_dim, FLAGS.num_relations,
-                    FLAGS.keep_prob, FLAGS.num_filters,
-                    FLAGS.lrn_rate, is_train=True)
+                    FLAGS.keep_prob, FLAGS.num_filters, FLAGS.attention_size,
+                    FLAGS.layer_size, FLAGS.keep_prob_rnn, FLAGS.lrn_rate, is_train=True)
   with tf.name_scope('Valid'):
-    with tf.variable_scope('CNNModel', reuse=True):
-      m_valid = CNNModel( word_embed, test_data, FLAGS.word_dim,
+    with tf.variable_scope('RNNModel', reuse=True):
+      m_valid = RNNModel( word_embed, test_data, FLAGS.word_dim,
                     FLAGS.pos_num, FLAGS.pos_dim, FLAGS.num_relations,
-                    1.0, FLAGS.num_filters,
-                    FLAGS.lrn_rate, is_train=False)
+                    1.0, FLAGS.num_filters, FLAGS.attention_size,
+                    FLAGS.layer_size, 1.0, FLAGS.lrn_rate, is_train=False)
   return m_train, m_valid
